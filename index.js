20048935ca984861db85e9b0ff7984eda22180d6
Tristan <trra53@gmail.com>
2018-06-30 15:50:11 +0800

    commit
    Co-Authored-By: tristangx <tristangx@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require("express")()
+const path = require("path")
+
+var port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
     res.sendFile("assets/html/index.js")
-    if (req.url != "/"){
+    if (req.url != "/") {
         res.sendFile(req.url)
     }
-}).listen(80)
+}).listen(port, function (err) {
+    console.log("Running on port:" + port)
+})
