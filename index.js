087cd3c9bb30aaa31f87ab3d150104f6450cbd58
Tristan <trra53@gmail.com>
2018-07-08 08:59:25 +0800

    commit
    Co-Authored-By: terriacas <terriacas@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path")
 var port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-    res.sendFile("/index.html")
+    res.send({hello : "world"})
 }).listen(port, function (err) {
     console.log("Running on port:" + port)
 })
